68571dab70694f9737b654c659689a0658f2ba23
Hessian_matrix <dima@zichuan-tech.com>
2025-06-10 16:11:51 +0800

    update build file for pdf

--- source/conf.py
+++ source/conf.py
@@ -5,11 +5,15 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
 
-project = 'baton_doc'
+project = 'Viobot2用户手册'
+# project = 'baton_doc'
 copyright = '2024, hessian'
-author = 'hessian'
-release = '0.0'
+author = 'HessianMatrix'
+release = '1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -31,3 +35,14 @@ language = "ch"
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+latex_engine = 'xelatex'
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+'preamble': r'''
+\addto\captionsenglish{\renewcommand{\chaptername}{}}
+\usepackage[UTF8, scheme = plain]{ctex}
+\setCJKmainfont{FandolSong-Regular.otf}
+'''
+}
