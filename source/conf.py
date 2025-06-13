6d20e0552e593b185873da5513fc6c0bfceb8b65
Hessian_matrix <dima@zichuan-tech.com>
2025-06-13 16:54:54 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ project = 'Viobot2用户手册'
 # project = 'baton_doc'
 copyright = '2024, hessian'
 author = 'HessianMatrix'
-release = '1.2'
+release = '1.21'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
